eb9fd299fb20f28ac07e4dbbd39cb714d7859cdc
AshleyNikr <arn4006@g.rit.edu>
2022-03-11 15:26:39 -0500

    Added boostrap

--- app.py
+++ app.py
@@ -5,7 +5,12 @@ app = Flask(__name__)
 
 @app.route('/')
 def hello_world():  # put application's code here
-    return render_template('index.html')
+    return "Hello, world!"
+
+
+@app.route('/welcome')
+def home():
+    return render_template("index.html")
 
 
 if __name__ == '__main__':
