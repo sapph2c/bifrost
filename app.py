b417b872c47dd8d086092d08c90f1972fbb3cfbb
AshleyNikr <arn4006@g.rit.edu>
2022-03-11 15:47:21 -0500

    Finished login page

--- app.py
+++ app.py
@@ -1,4 +1,4 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, redirect, url_for, request
 
 app = Flask(__name__)
 
@@ -11,6 +11,17 @@ def hello_world():  # put application's code here
 @app.route('/welcome')
 def home():
     return render_template("index.html")
+
+
+@app.route('/login', methods=['GET', 'POST'])
+def login():
+    error = None
+    if request.method == 'POST':
+        if request.form['username'] != 'admin' or request.form['password'] != 'admin':
+            error = "Invalid credentials. Please try again."
+        else:
+            return redirect(url_for('home'))
+    return render_template('login.html', error=error)
 
 
 if __name__ == '__main__':
